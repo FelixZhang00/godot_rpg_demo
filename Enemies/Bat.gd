7a77848f401376419a36117441d8ec694ad525fa
felix <l836828946@gmail.com>
2023-09-26 09:42:05 +0800

    seek

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -21,6 +21,8 @@ var knockback = Vector2.ZERO
 var velocity = Vector2.ZERO
 
 onready var stats =$Status
+onready var playerDetectionZone = $PlayerDetectionZone
+onready var sprite = $AnimatedSprite
 
 func _ready():
 	print(stats.max_health)
@@ -33,13 +35,24 @@ func _physics_process(delta):
 	match state:
 		IDLE:
 			velocity = velocity.move_toward(Vector2.ZERO,FRICTION*delta)
+			seek_player()
 		WANDER:
 			pass
 		CHASE:
-			pass		
+			var player = playerDetectionZone.player
+			if player!=null:
+				var direction = (player.global_position - global_position).normalized()
+				velocity = velocity.move_toward(direction*MAX_SPEED,ACCELERATION *delta)
+			else:
+				state = IDLE
+				
+			sprite.flip_h = velocity.x < 0		
+				
+	velocity = move_and_slide(velocity)		
 
 func seek_player():
-	pass
+	if playerDetectionZone.can_see_player():
+		state = CHASE
 
 func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage
